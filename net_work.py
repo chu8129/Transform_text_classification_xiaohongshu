f8309df7b77df0dc4ff9ece2fee58c9caa3de91a
wangqiuwen <chu8129@gmail.com>
2021-04-27 00:42:15 +0800

    update

--- net_work.py
+++ net_work.py
@@ -68,29 +68,33 @@ class Attention(Layer):
                 return inputs - (1 - mask) * 1e12
 
     def call(self, x):
-        if len(x) == 3:
-            Q_seq, K_seq, V_seq = x
-            Q_len, V_len = None, None
-        elif len(x) == 5:
-            Q_seq, K_seq, V_seq, Q_len, V_len = x
+        Q_seq, K_seq, V_seq = x
+        Q_len, V_len = None, None
+        print("build attention")
+
         Q_seq = K.dot(Q_seq, self.WQ)
         Q_seq = K.reshape(Q_seq, (-1, K.shape(Q_seq)[1], self.nb_head, self.size_per_head))
         Q_seq = K.permute_dimensions(Q_seq, (0, 2, 1, 3))
+
         K_seq = K.dot(K_seq, self.WK)
         K_seq = K.reshape(K_seq, (-1, K.shape(K_seq)[1], self.nb_head, self.size_per_head))
         K_seq = K.permute_dimensions(K_seq, (0, 2, 1, 3))
+
         V_seq = K.dot(V_seq, self.WV)
         V_seq = K.reshape(V_seq, (-1, K.shape(V_seq)[1], self.nb_head, self.size_per_head))
         V_seq = K.permute_dimensions(V_seq, (0, 2, 1, 3))
+
         A = K.batch_dot(Q_seq, K_seq, axes=[3, 3]) / self.size_per_head ** 0.5
         A = K.permute_dimensions(A, (0, 3, 2, 1))
         A = self.Mask(A, V_len, "add")
         A = K.permute_dimensions(A, (0, 3, 2, 1))
         A = K.softmax(A)
+
         O_seq = K.batch_dot(A, V_seq, axes=[3, 2])
         O_seq = K.permute_dimensions(O_seq, (0, 2, 1, 3))
         O_seq = K.reshape(O_seq, (-1, K.shape(O_seq)[1], self.output_dim))
         O_seq = self.Mask(O_seq, Q_len, "mul")
+
         return O_seq
 
     def compute_output_shape(self, input_shape):
@@ -105,7 +109,7 @@ def transfromer(n_symbols, config):
         input_length=config.batch_size,
     )(S_inputs)
     # embeddings = Position_Embedding()(embeddings)
-    O_seq = Attention(8, 16)([embeddings, embeddings, embeddings])
+    O_seq = Attention(8, 16)([embeddings, embeddings, embeddings,])
     O_seq = GlobalAveragePooling1D()(O_seq)
     O_seq = Dropout(Config.drop_rate)(O_seq)
     outputs = Dense(2, activation="softmax")(O_seq)
